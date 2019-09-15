db3101aa8903ba8a69be3b447b42416335427606
Mary Clayton <52017102+mary-clayton@users.noreply.github.com>
2019-09-14 21:53:43 -0700

    portfolio

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SideBar from './components/sidebar'
+import Intro from './components/intro'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+      <Intro />
+      </div>
+  );
+}
+
+export default App;
